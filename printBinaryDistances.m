ff43c9c60d15283584c14bf0c56e6175b7f9e185
seymaekren <seymaekren@std.sehir.edu.tr>
2013-09-12 15:57:53 +0300

    Update printBinaryDistances.m

--- printBinaryDistances.m
+++ printBinaryDistances.m
@@ -1,15 +1,16 @@
-function printBinaryDistances(COLIN, NTH, ALLDISTS);
+function printBinaryDistances(COLIN, NTH, ALLDISTS, inputModeIndex, inputModelIndex)
 
-fid = fopen('BinaryDistances.txt','w');
-fprintf(1, 'printing to BinaryDistances.txt\n');
+filename = sprintf('OutputFiles/1EF1/BinaryDistances_%d_model%d.txt',inputModeIndex, inputModelIndex);
+fid = fopen(filename,'w');
+fprintf(1, 'printing to %s\n', filename);
 for residue1Index = 1:length(COLIN)
-  for residue2Index =  1:length(COLIN)
-    if (ALLDISTS(COLIN(residue1Index),COLIN(residue2Index)) > NTH)
-      fprintf(fid, '0 ');
-    else
-      fprintf(fid, '1 ');
+    for residue2Index =  1:length(COLIN)
+        if (ALLDISTS(COLIN(residue1Index),COLIN(residue2Index)) > NTH)
+            fprintf(fid, '0 ');
+        else
+            fprintf(fid, '1 ');
+        end
     end
-  end	
-  fprintf(fid, '\n');
+    fprintf(fid, '\n');
 end
 fclose(fid);
